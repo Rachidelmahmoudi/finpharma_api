0ca3fefe95f9e2f7d1d2d86318c5b6bb5ab2542e
Rachid EL MAHMOUDI <35012638+Rachidelmahmoudi@users.noreply.github.com>
2026-04-30 01:31:04 +0100

    Change the import command

--- src/Command/ImportOpenPharmaciesCommand.php
+++ src/Command/ImportOpenPharmaciesCommand.php
@@ -27,88 +27,37 @@ class ImportOpenPharmaciesCommand extends Command
 
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
-        $jsonFile = $this->parameter_bag->get('kernel.project_dir') . '/public/data/pharmacies_complete.json';
+        $jsonFile = $this->parameter_bag->get('kernel.project_dir') . '/public/data/open.json';
         $data = json_decode(file_get_contents($jsonFile), true);
 
-        foreach ($data as $townData) {
-            $city = $townData['name'] ?? null;
-            foreach (($townData['neighborhoods'] ?? []) as $neighborhoodData) {
-                $dutyType = $neighborhoodData['dutyType'] ?? null;
-                foreach ($neighborhoodData['neighborhoods'] as $quartierData) {
-                    $quartier = !empty($quartierData['quartier']) ? str_replace('Quartier: ', '', $quartierData['quartier']) : 'Other';
-                    foreach ($quartierData['pharmacies'] as $pharmacyData) {
-                        $pharmacyName = $pharmacyData['pharmacy'] ?? null;
-                        $gardeStatus = $pharmacyData['garde_status'] ?? null;
-
-                        if (!$pharmacyName) {
-                            continue; // skip invalid data
-                        }
-
-                        // Find existing Pharmacy by name + city
-                        $pharmacy = $this->em
-                            ->getRepository(Pharmacy::class)
-                            ->findPharmacyByCity($pharmacyName, $city);
-
-                        $pharmacy = !empty($pharmacy) ?  $pharmacy[0] : null;
-
-                        if (!$pharmacy) {
-                            $output->writeln("Pharmacy not found: {$pharmacyName} in {$city}");
-                            continue;
-                        }
-                        $openPharmacy = $this->em
-                            ->getRepository(OpenPharmacy::class)
-                            ->findOneBy([
-                                'pharmacy' => $pharmacy->getId(),
-                                'day' => new \DateTime(),
-                                'source' => 'scraper'
-                            ]);
-                        if (!$openPharmacy) {
-                            $openPharmacy = new OpenPharmacy();
-                        }
-                        $openPharmacy->setPharmacy($pharmacy);
-                        $openPharmacy->setTown($quartier);
-                        $openPharmacy->setDutyType($dutyType);
-                        $openPharmacy->setCreatedAt(new \DateTimeImmutable('now'))->setUpdatedAt(new \DateTimeImmutable('now'));
-                        $openPharmacy->setGardeStatus(str_replace('Garde Jour ','', $gardeStatus))
-                        ->setDay(new \DateTime())
+        foreach ($data as $citiesData) {
+            $city = $citiesData['city'] ?? null;
+            $pharmaciesData = $citiesData['pharmacies'] ?? [];
+            foreach ($pharmaciesData as $pharmacyData) {
+                $pharmacyName = $pharmacyData['name'] ?? null;
+                $status = $pharmacyData['status'] ?? null;
+                $pharmacy = $this->em->getRepository(Pharmacy::class)->findOneBy([
+                    'name' => $pharmacyName,
+                    'city' => $city
+                ]);
+                if (!$pharmacy) {
+                    $pharmacy = new Pharmacy();
+                    $pharmacy->setName($pharmacyName)
+                        ->setAddress($pharmacyData['district'] ?? 'Mon adresse inconnue')
+                        ->setPhone('0600000000')
+                        ->setTown($pharmacyData['district'])
+                        ->setCity($city);
+                    $this->em->persist($pharmacy);
+                }
+                if ($status === 'Ouvert en ce moment') {
+                    $openPharmacy = new OpenPharmacy();
+                    $openPharmacy->setPharmacy($pharmacy)
+                        ->setTown($pharmacyData['district'])
+                        ->setGardeStatus($status)
+                        ->setCreatedAt(new \DateTimeImmutable())
                         ->setSource('scraper');
-                        if ($gardeStatus === "Ouvert en ce moment") {
-                            $openPharmacy->setAmFrom(new \DateTime('now'))->setAmTo(new \DateTime('now +3 hours'));
-                        }
-                        else if (str_contains($gardeStatus, '24h')) {
-                            $openPharmacy->setAmFrom(new \DateTime('today 00:00'))->setAmTo(new \DateTime('today 23:59'));
-                            $pharmacy->setIsAlwaysOpen(true);
-                        }
-                        else {
-                            preg_match('/(\d{1,2}h).*?(\d{1,2}h)/', $gardeStatus, $matches);
-                            if (count($matches) < 2) {
-                                continue; // skip if time format is not as expected
-                            }
-                            $start = $matches[1];
-                            $end = $matches[2];
-                            $start_hour = str_replace('h', ':00', $start);
-                            $end_hour = str_replace('h', ':00', $end);
-                            if (str_contains($gardeStatus, 'Garde Jour')) {
-                                if (new DateTime($end_hour) > new DateTime('today 12:00')) {
-                                    $openPharmacy->setAmFrom(new \DateTime("today {$start_hour}"))->setAmTo(new \DateTime("today 12:00"));
-                                    $openPharmacy->setPmFrom(new \DateTime("today 13:00"))->setPmTo(new \DateTime("today {$end_hour}"));    
-                                } else {
-                                    $openPharmacy->setAmFrom(new \DateTime("today {$start_hour}"))->setAmTo(new \DateTime("today {$end_hour}"));
-                                }
-                            } else if ($dutyType === 'garde-nuit') {
-                                dd(8);
-                                if (new DateTime($start_hour) > new DateTime($end_hour)) {
-                                    dd(1);
-                                    $openPharmacy->setPmFrom(new \DateTime("today {$start_hour}"))->setPmTo(new \DateTime("today 23:59"));
-                                    $openPharmacy->setAmFrom(new \DateTime("today 0:00"))->setAmTo(new \DateTime("today {$end_hour}"));
-                                } else {
-                                    dd(2);
-                                    $openPharmacy->setPmFrom(new \DateTime("today {$start_hour}"))->setPmTo(new \DateTime("today {$end_hour}")); 
-                                }
-                            }
-                        }
-                        $this->em->persist($openPharmacy);
-                    }
+                    
+                    $this->em->persist($openPharmacy);
                 }
             }
         }
